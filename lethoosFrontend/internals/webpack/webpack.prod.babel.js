74dc2a1d73327298297709f9be20aa07403b34fc
Raghav <raghav.agrawal@83incs.com>
2021-02-01 01:12:33 +0530

    favicon change and manifest description change for PWA

--- lethoosFrontend/internals/webpack/webpack.prod.babel.js
+++ lethoosFrontend/internals/webpack/webpack.prod.babel.js
@@ -121,9 +121,9 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'React Boilerplate',
-      short_name: 'React BP',
-      description: 'My React Boilerplate-based project!',
+      name: 'Le Thoos',
+      short_name: 'Le Thoos',
+      description: 'Online Food Ordering Website',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
